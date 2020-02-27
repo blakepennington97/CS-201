81295b02996e48a16dd3a13166dd9c0c48f0c637
blakepennington97 <47751437+blakepennington97@users.noreply.github.com>
2020-02-27 15:18:05 -0600

    Update main.cpp

--- phase2/main.cpp
+++ phase2/main.cpp
@@ -1,21 +1,26 @@
 #include "RBTree.cpp"
-int main()
-{
+#include <iostream>
+#include <stdio.h>
+
+int main() {
     RBTree tree;
 
-    tree.insert(7);
-    tree.insert(6);
-    tree.insert(5);
-    tree.insert(4);
-    tree.insert(3);
-    tree.insert(2);
-    tree.insert(1);
+    tree.insert('A', 1);
+    tree.insert('B', 2);
+    tree.insert('C', 3);
+    tree.insert('D', 4);
 
-    cout << "Inoder Traversal of Created Tree\n";
-    tree.inorder();
-
-    cout << "\n\nLevel Order Traversal of Created Tree\n";
-    tree.levelOrder();
+    tree.printTree();
 
     return 0;
 }
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
